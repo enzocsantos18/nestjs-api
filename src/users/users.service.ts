fe41dd71b46230780f0e2db6a3fd4de94dae5c45
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-22 09:25:50 -0300

    feat: Implementação da Listagem de usuários

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      nome: 'Enzo de Carvalho Santos',
+      email: 'enzocsantos18@gmail.com',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
