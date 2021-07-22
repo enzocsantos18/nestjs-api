853143ea8b10833f718cf94b98ad625d390de11d
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-22 10:26:28 -0300

    feat: Adição de validação

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(100)
+  nome: string;
+
+  @IsEmail()
+  email: string;
+}
